0311ab94727803687b6ae6bb5e952ddccb87b4f3
unknown <sergeyandmelnikov@gmail.com>
2022-03-25 23:48:31 +0300

    add TileList file

---
+++ src/app/Model/TileList.php
@@ -0,0 +1,47 @@
+<?php
+
+new SpecialTileCard(0, "Старт");
+new StreetCard(1, "Гран Виа", 60, "LightBlue");
+new OfficeCard(2, "Офис");
+new StreetCard(3, "Пласа Майор", 60, "LightBlue");
+new TaxCard(4, "Подоходный налог", 200, "or 10%");
+new MonopolyCard(5, "Торговый флот", 200);
+new StreetCard(6, "Гавайские острова", 100, "LightGreen");
+new ChanceCard(7, "Шанс");
+new StreetCard(8, "Таити", 100, "LightGreen");
+new StreetCard(9, "Фиджи", 120, "LightGreen");
+new SpecialTileCard(10, "Интерпол");
+new StreetCard(11, "Пьяцца Дель Пополо", 140, "Green");
+new MonopolyCard(12, "Рекламная компания", 150);
+new StreetCard(13, "Сант-Анджело", 140, "Green");
+new StreetCard(14, "Пьяцца Венеции", 160, "Green");
+new MonopolyCard(15, "Железнодорожная компания", 200);
+new StreetCard(16, "Трафальгар сквер", 180, "Yellow");
+new OfficeCard(17, "Офис", "Получите инструкции");
+new StreetCard(18, "Уайт Холл стрит", 180, "Yellow");
+new StreetCard(19, "Оксфорд стрит", 200, "Yellow");
+new SpecialTileCard(20, "Биржа");
+new StreetCard(21, "Улица Пятницкая", 220, "Red");
+new ChanceCard(22, "Шанс");
+new StreetCard(23, "Охотный Ряд", 220, "Red");
+new StreetCard(24, "Улица Тверская", 240, "Red");
+new MonopolyCard(25, "Автотранспортная компания", 200);
+new StreetCard(26, "Бульвар Мадлен", 260, "Blue");
+new StreetCard(27, "Елисейские поля", 260, "Blue");
+new MonopolyCard(28, "Телекомпания", 150);
+new StreetCard(29, "Сен-Дени", 280, "Blue");
+new SpecialTileCard(30, "Вы задержаны интерполом");
+new StreetCard(31, "Брайтон Бич", 300, "Orange");
+new StreetCard(32, "5-я Авеню", 300, "Orange");
+new OfficeCard(33, "Офис");
+new StreetCard(34, "Уолл-стриит", 320, "Orange");
+new MonopolyCard(35, "Авиа компания", 200);
+new ChanceCard(36, "Шанс");
+new StreetCard(37, "Ямато", 350, "Burgundy");
+new MonopolyCard(38, "Пожертвования в благотворительный фонд", 75);
+new StreetCard(39, "Минатоку", 460, "Burgundy");
+
+
+
+
+
