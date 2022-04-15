8b456290fcbe443715a16d8093c794fcb1d74b4b
unknown <sergeyandmelnikov@gmail.com>
2022-04-15 23:52:09 +0300

    added entry point file

---
+++ src/app/EnrtyPoint.php
@@ -0,0 +1,20 @@
+﻿namespace ManagersClub
+
+trait ManagersGame
+{
+	//...
+}
+
+# EnrtyPoint 
+class ManagerGame
+{
+	use ManagersGame;
+
+	function runGame() {
+		echo 'Start!'
+	}		
+}
+
+$game = new ManagerGame();
+$game->runGame();
+
